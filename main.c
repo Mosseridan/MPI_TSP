7ba20565080899d2da93f55fc1c410ab1f069d8e
Idan <mosseridan@gmail.com>
2017-11-26 16:54:09 +0200

    global vars static

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int tsp_main(int citiesNum, int xCoord[], int yCoord[], int shortestPath[]);
 int main(int argc, char** argv)
 {
 	// example input, with 18 cities. set citiesNum to less for an easier start.
-	int citiesNum = 11;
+	int citiesNum = 14;
 	int xCoord[] = {1, 3, 5, 9, 2,  3,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1};
 	int yCoord[] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 20, 22, 24, 26, 27, 29, 30, 31, 32};
 	int* shortestPath = malloc(citiesNum * sizeof(int));
@@ -44,7 +44,7 @@ int main(int argc, char** argv)
 		printf("%d\n", shortestPath[0]);
 		printf("Execution time: %g seconds\n", (double)(end - begin) / CLOCKS_PER_SEC);
 	}
-
+	printf("$\n");
 	MPI_Finalize ();	// shut down MPI - no need to do that in your code!
 	return 0;
 }
--- tsp_static.c
+++ tsp_static.c
@@ -4,18 +4,37 @@
 #include <limits.h>
 #include <string.h>
 
+#define CITIES_NUM_TAG 1
+#define X_COORD_TAG 2
+#define Y_COORD_TAG 3
+#define CURR_BOUND_TAG 4
+
+typedef struct {
+    double real,imag;
+} Path;
+
 
 int nCalls = 0;
+int citiesNum = 0;
+int* xCoord = 0;
+int* yCoord = 0;
+int shortestRes = INT_MAX;
+int* shortestPath = 0;
+int* visited = 0;
+int myRank = 0;
+int nProcs = 0;
+int nIters = 0;
 
+void TSPRec(int currBound, int currWeight, int level, int currPath[]);
 // comptes the distants value (wight) between cities i,j
-int dist(int xCoord[], int yCoord[], int i, int j)
+int dist(int i, int j)
 {
 	return abs(xCoord[i] - xCoord[j]) + abs(yCoord[i] - yCoord[j]);
 }
 
 // finds the first and second minimum edge costs
 // having an end at the vertex i
-int findMinEdges(int* first, int* second, int i, int citiesNum, int xCoord[], int yCoord[])
+int findMinEdges(int* first, int* second, int i)
 {
 	*first = INT_MAX;
 	*second = INT_MAX;
@@ -24,7 +43,7 @@ int findMinEdges(int* first, int* second, int i, int citiesNum, int xCoord[], in
 		if (i == j)
 			continue;
 
-		int distance = dist(xCoord, yCoord, i, j);
+		int distance = dist(i, j);
 		// printf("Distance between %d and %d is %d\n",i,j,distance);
 		if (distance <= *first)
 		{
@@ -40,37 +59,105 @@ int findMinEdges(int* first, int* second, int i, int citiesNum, int xCoord[], in
 }
 
 
-void printCurrPath(int citiesNum, int xCoord[], int yCoord[], int level, int currPath[]){
+void printCurrPath(int level, int currPath[]){
+	fflush(stdout);
+	printf("\n##[%d] currPath: ",myRank);
 	for (int i = 0; i < level-1; i++)
 	{
 		// print the city (and its distance from the next city in the path)
-		printf("%d (%d) ", currPath[i],
-				abs(xCoord[currPath[i]] - xCoord[currPath[(i + 1) % citiesNum]]) +
-				abs(yCoord[currPath[i]] - yCoord[currPath[(i + 1) % citiesNum]]) );
+		printf("%d (%d) ", currPath[i], dist(currPath[i],currPath[(i + 1) % citiesNum]));
 	}
-	printf("%d",(currPath[level-1]% citiesNum));
+	printf("%d\n",(currPath[level-1]% citiesNum));
+	fflush(stdout);
 }
 
-void printShortestPath(int citiesNum, int xCoord[], int yCoord[], int shortestPath[]){
+void printShortestPath(){
 	int minPathLen = 0;
+	fflush(stdout);
+	printf("\n##[%d] shortestPath: ",myRank);
 	for (int i = 0; i < citiesNum; i++)
 	{
-		minPathLen += abs(xCoord[shortestPath[i]] - xCoord[shortestPath[(i + 1) % citiesNum]]);
-		minPathLen += abs(yCoord[shortestPath[i]] - yCoord[shortestPath[(i + 1) % citiesNum]]);
+		// printf("\n\n#### [%d] %d %d %d\n\n",myRank,i, shortestPath, shortestPath[i]);
+		fflush(stdout);
+		minPathLen += dist(shortestPath[i], shortestPath[(i + 1) % citiesNum]);
 		// print the city (and its distance from the next city in the path)
-		printf("%d (%d) ", shortestPath[i],
-				abs(xCoord[shortestPath[i]] - xCoord[shortestPath[(i + 1) % citiesNum]]) +
-				abs(yCoord[shortestPath[i]] - yCoord[shortestPath[(i + 1) % citiesNum]]) );
+
+		printf("%d (%d) ", shortestPath[i], dist(shortestPath[i],shortestPath[(i + 1) % citiesNum]));
 	}
-	printf("%d  [%d]", shortestPath[0],minPathLen);
+	printf("%d  [%d]\n", shortestPath[0],minPathLen);
+	fflush(stdout);
 }
 
-void printVisited(int citiesNum, int visited[]){
+
+void printVisited(){
+	fflush(stdout);
+	printf("\n##[%d] visited: ",myRank);
 	for (int i = 0; i < citiesNum; i++)
 	{
 		printf("%d (%d) ",i, visited[i]);
 	}
+	printf("\n");
+	fflush(stdout);
 }
+
+
+void computeBranch(int i, int currBound, int currWeight, int level, int currPath[])
+{
+
+	int distance = dist(currPath[level-1], i);
+
+	// Consider next vertex if it is not same (diagonal
+	// entry in adjacency matrix and not visited
+	// already)
+	if(distance != 0 && !visited[i])
+	{
+
+		// printf("!![%d] Trying: ",myRank);
+		// printCurrPath(citiesNum,xCoord,yCoord,level,currPath);
+		// printf(" -> (%d) %d \n",distance,i);
+
+		int newBound = currBound;
+		int newWeight = currWeight + distance;
+		// printf("@@@@ from: %d, to: %d, distance: %d, currWeight: %d, newWeight: %d\n",currPath[level-1], i,distance, currWeight, newWeight);
+		int firstMin, secondMin, prevFirstMin, prevSecondMin;
+		findMinEdges(&firstMin, &secondMin, i);
+		findMinEdges(&prevFirstMin, &prevSecondMin, currPath[level-1]);
+
+		// different computation of curr_bound for
+		// level 2 from the other levels
+		if (level == 1){
+			newBound -= ((prevFirstMin + firstMin)/2);
+		}
+		else{
+			newBound -= ((prevSecondMin + firstMin)/2);
+		}
+
+		// newBound + currWeight is the actual lower bound
+		// for the node that we have arrived on
+		// If current lower bound < shortestRes, we need to explore
+		// the node further
+		if (newBound + newWeight < shortestRes)
+		{
+			// printf("!![%d] %d (newBound) + %d (newWeight) = %d < %d (shortestRes) *** Exploring branch ***\n\n",myRank,newBound,newWeight,newBound+newWeight,*pShortestRes);
+			currPath[level] = i;
+			visited[i] = 1;
+
+			// call TSPRec for the next level
+			TSPRec(newBound, newWeight, level+1, currPath); //TODO: currPath and visited might need deep copy
+		}
+		else{
+			// printf("!![%d] %d (newBound) + %d (newWeight) = %d >= %d (shortestRes) $$ Cuting branch $$\n\n",myRank,newBound,newWeight,newBound+newWeight,*pShortestRes);
+		}
+
+		// Also reset the visited array //TODO might want to deep copy at call instead
+		memset(visited, 0, citiesNum*sizeof(int));
+		for (int j=0; j<=level-1; j++)
+		{
+			visited[currPath[j]] = 1;
+		}
+	}
+}
+
 
 // function that takes as arguments:
 // curr_bound -> lower bound of the root node
@@ -79,39 +166,24 @@ void printVisited(int citiesNum, int visited[]){
 //         space tree
 // curr_path[] -> where the solution is being stored which
 //                would later be copied to final_path[]
-void TSPRec(
-	int citiesNum,
-	int xCoord[],
-	int yCoord[],
-	int currBound,
-	int currWeight,
-	int level,
-	int currPath[],
-	int* pShortestRes,
-	int shortestPath[],
-	int visited[])
+void TSPRec(int currBound, int currWeight, int level, int currPath[])
 {
 
 	nCalls++;
-	printf("@@ TSPRec level: %d, currWeight: %d, currBound: %d",level,currWeight, currBound);
-	printf("\n## currPath: ");
-	printCurrPath(citiesNum,xCoord,yCoord,level,currPath);
-
-	printf("\n## shortestPath: ");
-	printShortestPath(citiesNum,xCoord,yCoord,shortestPath);
-	printf(" [%d]",*pShortestRes);
-
-	printf("\n## visited: ");
-	printVisited(citiesNum, visited);
-	printf("\n\n");
-
+	// fflush(stdout);
+	// printf("\n@@[%d] TSPRec level: %d, currWeight: %d, currBound: %d, shortestRes: %d\n",myRank,level,currWeight, currBound, shortestRes);
+	// printCurrPath(level,currPath);
+	// printShortestPath(shortestPath);
+	// printVisited();
+	// printf("\n\n");
+	// fflush(stdout);
 
   // base case is when we have reached level N which
   // means we have covered all the nodes once
   if (level == citiesNum)
   {
 
-		int distance = dist(xCoord, yCoord, currPath[level-1], currPath[0]);
+		int distance = dist(currPath[level-1], currPath[0]);
 
     // check if there is an edge from last vertex in
     // path back to the first vertex
@@ -124,15 +196,14 @@ void TSPRec(
 
       // Update final result and final path if
       // current result is better.
-      if (currRes < *pShortestRes)
+      if (currRes < shortestRes)
       {
-				printf("\n!! Found new shortest path that is %d long\n",currRes);
-        // copyToFinal(currPath); // TODO: make parallel
+				// printf("\n!![%d] Found new shortest path that is %d long\n",myRank,currRes);
 				for (int i= 0; i < citiesNum; i++)
 						shortestPath[i] = currPath[i];
 				shortestPath[citiesNum] = currPath[0];
 
-        *pShortestRes = currRes;  // TODO: make parallel
+        shortestRes = currRes;
       }
     }
     return;
@@ -142,117 +213,114 @@ void TSPRec(
 	// build the search space tree recursively
 	for (int i = 0; i < citiesNum; i++)
 	{
-		int distance = dist(xCoord, yCoord, currPath[level-1], i);
-
-	  // Consider next vertex if it is not same (diagonal
-    // entry in adjacency matrix and not visited
-    // already)
-		if(distance != 0 && !visited[i])
-    {
-
-			printf("!! Trying: ");
-			printCurrPath(citiesNum,xCoord,yCoord,level,currPath);
-			printf(" -> (%d) %d \n",distance,i);
-
-			int newBound = currBound;
-			int newWeight = currWeight + distance;
-			// printf("@@@@ from: %d, to: %d, distance: %d, currWeight: %d, newWeight: %d\n",currPath[level-1], i,distance, currWeight, newWeight);
-			int firstMin, secondMin, prevFirstMin, prevSecondMin;
-			findMinEdges(&firstMin, &secondMin, i, citiesNum, xCoord, yCoord);
-			findMinEdges(&prevFirstMin, &prevSecondMin, currPath[level-1], citiesNum, xCoord, yCoord);
-
-      // different computation of curr_bound for
-      // level 2 from the other levels
-      if (level == 1){
-				newBound -= ((prevFirstMin + firstMin)/2);
-			}
-      else{
-				newBound -= ((prevSecondMin + firstMin)/2);
-			}
-
-      // newBound + currWeight is the actual lower bound
-      // for the node that we have arrived on
-      // If current lower bound < shortestRes, we need to explore
-      // the node further
-      if (newBound + newWeight < *pShortestRes)
-      {
-				printf("!! %d (newBound) + %d (newWeight) = %d < %d (shortestRes) *** Exploring branch ***\n\n",newBound,newWeight,newBound+newWeight,*pShortestRes);
-				currPath[level] = i;
-        visited[i] = 1;
-
-        // call TSPRec for the next level
-        TSPRec(citiesNum, xCoord, yCoord, newBound, newWeight, level+1, currPath, pShortestRes, shortestPath, visited); //TODO: currPath and visited might need deep copy
-      }
-			else{
-				printf("!! %d (newBound) + %d (newWeight) = %d >= %d (shortestRes) $$ Cuting branch $$\n\n",newBound,newWeight,newBound+newWeight,*pShortestRes);
-			}
-
-      // Also reset the visited array //TODO might want to deep copy at call instead
-      memset(visited, 0, citiesNum*sizeof(int));
-      for (int j=0; j<=level-1; j++)
-			{
-				visited[currPath[j]] = 1;
-			}
-		}
+		computeBranch(i, currBound, currWeight, level, currPath);
 	}
 }
 
-
 // The static parellel algorithm main function.
-int tsp_main(int citiesNum, int xCoord[], int yCoord[], int shortestPath[])
+int tsp_main(int _citiesNum, int _xCoord[], int _yCoord[], int _shortestPath[])
 {
-
-	int myRank, procCount;
-	// Stores the final minimum weight of shortest tour.
-	int shortestRes = INT_MAX;
-	// visited[] keeps track of the already visited nodes
-	// in a particular path
-	int visited[citiesNum];
+	MPI_Request request;
+	MPI_Status status;
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-	MPI_Comm_size(MPI_COMM_WORLD, &procCount);
+	MPI_Comm_size(MPI_COMM_WORLD, &nProcs);
+
+	struct {
+		 int val;
+		 int rank;
+	} finalRes, myRes;
+	finalRes.val =  INT_MAX;
+	finalRes.rank = myRank;
+	myRes.val = INT_MAX;
+	myRes.rank = myRank;
+	int currBound = 0;
 
 	if(myRank == 0)
 	{
-		int currPath[citiesNum+1];
+		citiesNum = _citiesNum;
+		xCoord = malloc(citiesNum*sizeof(int));
+		yCoord = malloc(citiesNum*sizeof(int));
 
-		// Calculate initial lower bound for the root node
-		// using the formula 1/2 * (sum of first min +
-		// second min) for all edges.
-		// Also initialize the currPath and visited array
-		int currBound = 0;
-		memset(currPath, -1, citiesNum*sizeof(int));
-		memset(visited, 0, citiesNum*sizeof(int));
+		memcpy(xCoord, _xCoord, citiesNum*sizeof(int));
+		memcpy(yCoord, _yCoord, citiesNum*sizeof(int));
+
+		for(int i = 1; i < nProcs; i++)
+		{
+			MPI_Isend(&citiesNum, 1, MPI_INT, i, CITIES_NUM_TAG, MPI_COMM_WORLD, &request);
+			MPI_Isend(xCoord, citiesNum, MPI_INT, i, X_COORD_TAG, MPI_COMM_WORLD, &request);
+			MPI_Isend(yCoord, citiesNum, MPI_INT, i, Y_COORD_TAG, MPI_COMM_WORLD, &request);
+		}
 
 		// Compute initial bound
 		for (int i = 0; i < citiesNum; i++)
 		{
 			int first, second;
-			findMinEdges(&first, &second, i, citiesNum, xCoord, yCoord);
+			findMinEdges(&first, &second, i);
 			currBound += (first + second);
 		}
 
 		// Rounding off the lower bound to an integer
 		currBound = (currBound&1)? currBound/2 + 1 : currBound/2;
 
-
-		// We start at vertex 1 so the first vertex
-		// in curr_path[] is 0
-		visited[0] = 1;
-		currPath[0] = 0;
-
-		// Call to TSPRec for curr_weight equal to
-		// 0 and level 1
-		TSPRec(citiesNum, xCoord, yCoord, currBound, 0, 1, currPath, &shortestRes, shortestPath, visited);
-
-		int factorial = 1;
-		for(int i = 1; i <= citiesNum; i++){
-			factorial = factorial * i;
+		for(int i = 1; i < nProcs; i++)
+		{
+			MPI_Isend(&currBound, 1, MPI_INT, i, CURR_BOUND_TAG, MPI_COMM_WORLD, &request);
 		}
-		int speedup = factorial/nCalls;
-		printf("\n\n@@ Total number of calls: %d instead of %d. ~ %d Times faseter\n\n",nCalls,factorial,speedup,speedup*100);
+	}
+	else {
+		MPI_Recv(&citiesNum, 1, MPI_INT, 0, CITIES_NUM_TAG, MPI_COMM_WORLD, &status);
+		xCoord = malloc(citiesNum*sizeof(int));
+		yCoord = malloc(citiesNum*sizeof(int));
+		MPI_Irecv(xCoord, citiesNum, MPI_INT, 0, X_COORD_TAG, MPI_COMM_WORLD, &request);
+		MPI_Irecv(yCoord, citiesNum, MPI_INT, 0, Y_COORD_TAG, MPI_COMM_WORLD, &request);
+		MPI_Irecv(&currBound, 1, MPI_INT, 0, CURR_BOUND_TAG, MPI_COMM_WORLD, &request);
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
 
+	int currPath[citiesNum+1];
+	shortestPath = _shortestPath;
+	// shortestPath = malloc(citiesNum*sizeof(int));
+	visited = malloc(citiesNum*sizeof(int));
+	memset(currPath, -1, citiesNum*sizeof(int));
+	// memset(shortestPath, 0, citiesNum*sizeof(int));
+	memset(visited, 0, citiesNum*sizeof(int));
+	// We start at vertex 1 so the first vertex
+	// in curr_path[] is 0
+	visited[0] = 1;
+	currPath[0] = 0;
+	nIters = citiesNum/nProcs;
+
+
+	// fflush(stdout);
+	// printf("\n\n\n$@#@#@#@$$ [%d] %d\n",myRank,citiesNum);
+	// for(int i = 0; i < citiesNum; i++){
+	// 	printf("%d:%d ",xCoord[i], yCoord[i]);
+	// }
+	// printf("\n");
+	// for(int i = 0; i < citiesNum; i++){
+	// 	printf("%d",shortestPath[i]);
+	// }
+	// printf("\n\n");
+	// fflush(stdout);
+
+
+
+	int start = myRank * nIters;
+	int end = (myRank == nProcs-1 ? citiesNum : start + nIters);
+	for(int i = start; i < end; i++)
+	{
+		computeBranch(i, currBound, 0, 1, currPath);
 	}
 
-	return shortestRes;
+	myRes.val = shortestRes;
+	MPI_Allreduce(&myRes, &finalRes, 1, MPI_2INT, MPI_MINLOC, MPI_COMM_WORLD);
+	MPI_Bcast(shortestPath, citiesNum, MPI_INT,finalRes.rank, MPI_COMM_WORLD);
+
+	free(xCoord);
+	free(yCoord);
+	// free(shortestPath);
+	free(visited);
+	MPI_Barrier(MPI_COMM_WORLD);
+	return finalRes.val;
 }
